41b1d8c9f3068e9aae5728793f43ea19ab71f84f
Edmund Tsang <edmundcy@connect.hku.hk>
2023-02-28 18:56:03 +0800

    nan

--- VaR.py
+++ VaR.py
@@ -6,17 +6,17 @@ def stochastic_model_dividend(s_zero, mu, sigma, T, Z, delta):
     T, the random variable Z, and the dividend delta."""
     return s_zero * math.exp((mu - delta - sigma**2/2)*T + sigma * math.sqrt(T) * Z)
 
-def expected_value_divident(s_zero, mu, sigma, T, delta):
+def expected_value_divident(s_zero, mu, T, delta):
     """Returns the expected value of a stock price at time T given the
     initial stock price s_zero, the drift mu, the volatility sigma,
     the time T, and the dividend delta."""
-    return s_zero * math.exp((mu - delta - sigma**2/2)*T)
+    return s_zero * math.exp((mu - delta)*T)
 
 def relative_VaR(s_zero, mu, sigma, T, Z, delta):
     """Returns the relative value at risk of a stock price at time T
     given the initial stock price s_zero, the drift mu, the volatility
     sigma, the time T, the random variable Z, and the dividend delta."""
-    return (expected_value_divident(s_zero, mu, sigma, T, delta) - (stochastic_model_dividend(s_zero, mu, sigma, T, Z, delta) + 0.5/52))
+    return (expected_value_divident(s_zero, mu, T, delta) - (stochastic_model_dividend(s_zero, mu, sigma, T, Z, delta) + 0.5/52))
 
 def absolute_VaR(s_zero, mu, sigma, T, Z, delta):
     """Returns the absolute value at risk of a stock price at time T
@@ -35,7 +35,7 @@ def main():
     D = 0.5
     delta = math.log((s_zero + D)/s_zero)
     print("The worst price of the stock at time T is:", stochastic_model_dividend(s_zero, mu, sigma, T, Z, delta) + D/52)
-    print("The expected value of the portfolio is:", expected_value_divident(s_zero, mu, sigma, T, delta))
+    print("The expected value of the portfolio is:", expected_value_divident(s_zero, mu, T, delta))
     print("The relative value at risk is:", num_shares*relative_VaR(s_zero, mu, sigma, T, Z, delta))
     print("The absolute value at risk is:", num_shares*absolute_VaR(s_zero, mu, sigma, T, Z, delta))
 
